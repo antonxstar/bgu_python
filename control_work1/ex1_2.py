ee458c98750ff6a2d38d3c41f6b4bae2964d4b44
antonxstar <antonx.starovoytov@gmail.com>
2023-02-22 21:50:18 +0300

    commit V3

--- control_work1/ex1_2.py
+++ control_work1/ex1_2.py
@@ -1,6 +1,10 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-print(max(a,b,c,d))
+a = float(input("Введите a : ")) # Вводим число a
+b = float(input("Введите b : ")) # Вводим число b
+if a>b:
+   c = (b/a)*100
+   cr = round(c, 2)
+   print(cr)
+if a<b:
+   c = (a/b)*100
+   cr = round(c, 2)
+   print(cr)
---
+++ control_work1/ex1_4.py
@@ -0,0 +1,4 @@
+city1 = str(input())
+city2 = str(input())
+city3 = str(input())
+print(city3+', '+city2+', '+city1)
